6cc915d1b26c5f1325a05dc23ebfa065320130e9
Sunny2920 <t104590454@ntut.org.tw>
2017-03-03 10:42:30 +0800

    ㄏ

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -12,9 +12,13 @@ bool Guess(int number)
         target = r.Next() % 100 + 1;
     }
 
-    if (number == target)return true;
-
-    return false;
+	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
+		return true;
+	}
+	else std::cout << "Wrong" << std::endl;
+	return false;
 }
 int main(array<System::String^>^ args)
 {
